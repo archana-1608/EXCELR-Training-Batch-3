4d7db30cff3ed9b04d6f9f81c27347be77fdf8f3
archana-1608 <archanareddy8333@gmail.com>
2025-01-03 11:01:17 +0530

    Add files via upload

---
+++ Demo005.java
@@ -0,0 +1,10 @@
+public class Demo005{
+    public static void main(String[] args){
+        String name = "Pandu";
+        String pwd = "Pandu123";
+        while((name=="Pandu")&&(pwd=="Pandu123")){
+            System.out.println("Hello. Welcome");
+            break;
+        }
+    }
+}
