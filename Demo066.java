c25db2efbb48a541d068d5835b96f4b350c82ec8
archana-1608 <archanareddy8333@gmail.com>
2025-02-05 12:40:46 +0530

    Index values of an array

---
+++ Demo066.java
@@ -0,0 +1,10 @@
+public class Demo066 {
+    public static void main(String[] args) {
+        int[] ar={10,20,30,40,50};
+        System.out.println("Array Index values: ");
+        for(int i=0; i<ar.length;i++){
+            System.out.println("Index"+i+":"+ar[i]);
+        }
+    }
+
+}
