b1d77d439f1228fba69deb2428543ccbe1b0d45c
archana-1608 <archanareddy8333@gmail.com>
2025-02-05 12:40:13 +0530

    Array Elements

---
+++ Demo065.java
@@ -0,0 +1,11 @@
+public class Demo065 {
+    public static void main(String[] args) {
+        int[] ar={10,20,30,40,50};
+        System.out.println("Array Elements: ");
+        for(int a:ar){
+            System.out.print(a+" ");
+        }
+
+    }
+
+}
