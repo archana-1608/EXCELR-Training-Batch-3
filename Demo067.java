a8dfd0c5c90d91837629ad0c6514e54a78eae532
archana-1608 <archanareddy8333@gmail.com>
2025-02-06 12:20:19 +0530

    Displaying Even Elements in an Array

---
+++ Demo067.java
@@ -0,0 +1,11 @@
+public class Demo067 {
+    public static void main(String[] args) {
+        int[] ar={10,20,30,40,50};
+        System.out.println("Even Elements in array: ");
+        for(int i=2;i<ar.length;i+=2){
+                System.out.println(ar[i]+" ");
+            
+        }
+    }
+
+}
